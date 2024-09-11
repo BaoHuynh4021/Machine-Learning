d596dfb337d8244da3cf7d4820fac341b53f5d6f
BaoHuynh4021 <110713340+BaoHuynh4021@users.noreply.github.com>
2024-09-11 22:50:30 +0700

    Add files via upload

--- Linear Regression.py
+++ Linear Regression.py
@@ -10,18 +10,19 @@ def grad(x):
     return 6*x + 2 + 4*np.cos(x)
 
 def gradient_descent(x0, eta, limit, loop_max):
-    x = x0
+    x = [x0]
     for i in range(loop_max):
-        x = x - eta*grad(x)
-        if abs(grad(x)) < limit:
+        x_new = x[-1] - eta*grad(x[-1])
+        if abs(grad(x_new)) < limit:
             print("\nConvergence at loop no:", i)
-            return np.round(x, 6), i
+            return np.round(x_new, 6), i
+        x.append(x_new)
     print("\nexecute failed") # can not find convergence point after 10000 loops 
-    return np.round(x, 6), loop_max
+    return np.round(x_new, 6), loop_max
 
 # Define parameters
-x_start = -1       # x0
-eta = 0.01          # learning rate
+x_start = 5       # x0
+eta = 0.1          # learning rate
 limit = 1e-3       # convergence threshold |f'(x)| < 1e-3
 loop_max = 10000   # max loop executable     
 
